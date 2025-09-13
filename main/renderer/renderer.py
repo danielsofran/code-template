20e25a5931a3cf2a2fe65c6f8dd73210a440d458
Daniel <daniel.sofran@stud.ubbcluj.ro>
2025-09-13 23:55:16 +0300

    Allow wildcards for file names nested wildcards, no functions no arrays
    Issue: GEN-94

--- main/renderer/renderer.py
+++ main/renderer/renderer.py
@@ -1,4 +1,7 @@
 import os, shutil, importlib.util
+import re
+from typing import List, Tuple, Any, Dict
+
 from jinja2 import Environment, FileSystemLoader
 
 from jinjaGenerator.settings import BASE_DIR
@@ -72,6 +75,48 @@ def __create_jinja_env(template_folder):
         env.globals[name] = func
     return env
 
+def __process_wildcard(wildcard: str, context: dict, env: Environment) -> Tuple[str, Dict[str, Any]]:
+    """
+    Process a wildcard and return the resolved string and additional context.
+    """
+    if not '.' in wildcard and not '(' in wildcard:
+        # simple case. just context.wildcard
+        if wildcard in context:
+            return str(context[wildcard]), {wildcard: str(context[wildcard])}
+        else:
+            raise ValueError(f"Wildcard '{wildcard}' not found in context")
+    # complex case 1. nested context like model.name
+    elif '.' in wildcard and not '(' in wildcard:
+        parts = wildcard.split('.')
+        value = context.copy()
+        for part in parts:
+            if part in value:
+                value = value[part]
+            else:
+                raise ValueError(f"Wildcard '{wildcard}' not found in context")
+        return str(value), {wildcard: str(value)}
+    else: raise ValueError(f"Invalid wildcard format: {wildcard}")
+
+def __resolve_path(path: str, context: dict, env: Environment) -> List[Tuple[str, Dict[str, Any]]]:
+    """
+    Resolve a path with possible wildcards using the context.
+    Returns a list of tuples (resolved_path, additional_context)
+    """
+    if '%' not in path:
+        return [(path, {})]
+    final_additional_context = {}
+    path = path[:]
+    while path.find('%') != -1:
+        start_index = path.find('%')
+        end_index = path.find('%', start_index + 1)
+        if end_index == -1:
+            break
+        wildcard = path[start_index + 1:end_index]
+        resolved_str, additional_context = __process_wildcard(wildcard, context, env)
+        final_additional_context.update(additional_context)
+        path = path[:start_index] + resolved_str + path[end_index + 1:]
+    return [(path, final_additional_context)]
+
 def render(template_folder, context):
     project_folder = os.path.join(template_folder, 'project')
     env = __create_jinja_env(template_folder)
@@ -83,21 +128,27 @@ def render(template_folder, context):
         for file in files:
             template_path = os.path.relpath(os.path.join(root, file), project_folder)
             template_path = str(template_path).replace('\\', '/')
-            if file.endswith('.jinja') or file.endswith('.j2'):
-                template = env.get_template(template_path)
-                output = template.render(context)
-                # determine the output path
-                relative_path = os.path.relpath(os.path.join(root, file), project_folder)
-                relative_path = relative_path.replace('.jinja', '').replace('.j2', '')
-                output_path = os.path.join(output_folder, relative_path)
-                output_dir = os.path.dirname(output_path)
-                os.makedirs(output_dir, exist_ok=True)
-                with open(output_path, 'w') as f:
-                    f.write(output)
-            else: # copy the file as is
-                source_path = os.path.join(root, file)
-                relative_path = os.path.relpath(source_path, project_folder)
-                output_path = os.path.join(output_folder, relative_path)
-                output_dir = os.path.dirname(output_path)
-                os.makedirs(output_dir, exist_ok=True)
-                shutil.copy(source_path, output_path)
+            resolved_paths = __resolve_path(file, context, env)
+            for resolved_path, additional_context in resolved_paths:
+                if resolved_path.endswith('.jinja') or resolved_path.endswith('.j2'):
+                    template = env.get_template(template_path)
+                    full_context = context.copy()
+                    full_context.update(additional_context)
+                    output = template.render(full_context)
+                    # determine the output path
+                    # TODO: this can be modified in the output. to think a solution
+                    relative_path = os.path.relpath(os.path.join(root, resolved_path), project_folder)
+                    relative_path = relative_path.replace('.jinja', '').replace('.j2', '')
+                    output_path = os.path.join(output_folder, relative_path)
+                    output_dir = os.path.dirname(output_path)
+                    os.makedirs(output_dir, exist_ok=True)
+                    with open(output_path, 'w') as f:
+                        f.write(output)
+                else: # copy the file as is
+                    source_path = os.path.join(root, file)
+                    # TODO; this won't work for folders with wildcards
+                    relative_path = os.path.join(os.path.dirname(os.path.relpath(source_path, project_folder)), resolved_path)
+                    output_path = os.path.join(output_folder, relative_path)
+                    output_dir = os.path.dirname(output_path)
+                    os.makedirs(output_dir, exist_ok=True)
+                    shutil.copy(source_path, output_path)
